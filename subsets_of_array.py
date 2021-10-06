a3f83cee010be8dbe1c7d24c2b379ebd4caa2054
sai_sudha <saisudhavadisina@gmail.com>
2021-10-06 21:03:35 +0530

    Finds all possible subsets of the given array and prints it to the console in a lexicographical order

---
+++ subsets_of_array.py
@@ -0,0 +1,26 @@
+def solve(A, tem, p, res):
+    varlist, n = list(tem), len(A)
+    res.append(varlist)
+    for i in range(p, n):
+        if i > p and A[i] == A[i - 1]:
+            continue
+        else:
+            tem.append(A[i])
+            solve(A, tem, i + 1, res)
+            tem.pop()
+    return
+    
+def subsets(A):
+    res = []
+    solve(sorted(A), [], 0, res)
+    res = [i for i in res]
+    res.remove([])
+    return "\n".join(" ".join(str(j) for j in i) for i in res)
+
+if __name__ == "__main__":
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        ar = list(map(int, input().split()))
+        print(subsets(ar))
+        print()
